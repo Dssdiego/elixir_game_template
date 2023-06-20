9e50487c0dccbd0abd28b70e84783a217e8c3c36
Diego Santos Seabra <dss-diego@hotmail.com>
2023-06-19 23:11:32 -0300

    animated sprites

--- src/MyGame.cpp
+++ src/MyGame.cpp
@@ -10,6 +10,7 @@
 #include "../../src/engine/rendering/Camera.h"
 #include "../../src/engine/components/Transform.h"
 #include "../../src/engine/components/Sprite.h"
+#include "../../src/engine/components/AnimatedSprite.h"
 
 using namespace Feather;
 
@@ -22,18 +23,52 @@ void MyGame::Init()
 	Camera::SetWorldPosition(glm::vec3(3.25f, 0.8f, 0.f));
 
 	auto bird = ECS::Create("Bird");
-
-	Sprite sprComp =
+	AnimatedSprite birdAnimSpr =
 	{
 		Color::White,
 		"sprite",
 		"game_sprites",
+		0,
+		5,
+		0.1f,
+		0.f,
 		1,
 		true
 	};
-
 	ECS::AddComponent(bird, Transform{});
-	ECS::AddComponent(bird, sprComp);
+	ECS::AddComponent(bird, birdAnimSpr);
+
+	auto bomb = ECS::Create("Bomb");
+	AnimatedSprite bombAnimSpr =
+	{
+		Color::White,
+		"sprite",
+		"game_sprites",
+		10,
+		12,
+		0.1f,
+		0.f,
+		10,
+		true
+	};
+	ECS::AddComponent(bomb, Transform{});
+	ECS::AddComponent(bomb, bombAnimSpr);
+
+	auto explosion = ECS::Create("Explosion");
+	AnimatedSprite explosionAnimSpr =
+	{
+		Color::White,
+		"sprite",
+		"game_sprites",
+		16,
+		21,
+		0.1f,
+		0.f,
+		16,
+		true
+	};
+	ECS::AddComponent(explosion, Transform{});
+	ECS::AddComponent(explosion, explosionAnimSpr);
 }
 
 void MyGame::Update()
